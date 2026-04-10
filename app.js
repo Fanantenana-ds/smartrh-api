509677d2c60b65d286837a471b80763f33dd10ba
Fanantenana-ds <fanantenana7170@gmail.com>
2026-04-10 15:10:19 +0200

    feat: add payroll calculation (step 1 - intentional debt)

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const payrollRoutes = require('./routes/payroll');
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use('/api', payrollRoutes);
 
 app.get('/', (req, res) => {
   res.send('SmartHR API is running');
